e69c30774b97ee4e99f796d40b44f8eeeb74a248
Sredoje Cutovic <sredojecutovic@gmail.com>
2022-12-26 20:10:43 +0100

    Improvements to quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -6,4 +7,8 @@ Rails.application.routes.draw do
   # config/routes.rb
 
   resources :quotes
+
+  # config/routes.rb
+
+  root to: "pages#home"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.create! :email => 'accountant@kpmg.com', :password => 'password', :password_confirmation => 'password', :company => Company.all.first
+user1 = User.create! :email => 'manager@kpmg.com', :password => 'password', :password_confirmation => 'password', :company => Company.all.first
+user2 = User.create! :email => 'eavesdropper@pwc.com', :password => 'password', :password_confirmation => 'password', :company => Company.all.last
